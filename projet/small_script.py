dafc18fa8563af6c1994e26ad20b2c082a245689
jtib <jtib@ntymail.com>
2017-11-21 17:14:00 -0500

    je suis fatiguée

--- projet/small_script.py
+++ projet/small_script.py
@@ -7,20 +7,17 @@ with open('dataset_ubicomp2013/dataset_ubicomp2013_checkins.txt') as dataset:
 content = [x.strip() for x in content]
 content = [x.replace('\t', ' ') for x in content]
 content = [x.split() for x in content]
-# formatting (takes a looong time)
-i = 0
-vec = []
-while i < len(content):
-    user = sorted(content)[i][0]
-    v = [user]
-    while i < len(content) and sorted(content)[i][0] == user:
-        v.append(sorted(content)[i][1])
-        i += 1
-    vec.append(v)
+# formatting
+dic = {}
+for (k,v) in content:
+    if k in dic:
+        dic[k].append(v)
+    else:
+        dic[k] = [v]
 
 # Window size
-w = max([len(x) for x in vec])
+w = max([len(x) for x in dic.items()])
 
 # Model (inc. training (CBOW))
-model = gensim.models.Word2Vec(vec, window=w, min_count=1, size=50)
+model = gensim.models.Word2Vec(dic, window=w, min_count=1, size=50)
 
