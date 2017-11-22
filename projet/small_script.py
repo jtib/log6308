5b5531fde8ca58c67e973efccf8292163933c90b
jtib <jtib@ntymail.com>
2017-11-21 23:14:40 -0500

    test model (working)

--- projet/small_script.py
+++ projet/small_script.py
@@ -1,5 +1,6 @@
 import gensim
 import numpy as np
+import matplotlib.pyplot as plt
 
 # with dataset_ubicomp
 with open('dataset_ubicomp2013/dataset_ubicomp2013_checkins.txt') as dataset:
@@ -44,5 +45,19 @@ model = gensim.models.Word2Vec(vec, window=w, min_count=1, size=16, batch_words=
 users = [model.wv[u] for u in vec]
 
 # Test (article)
+locs_test = ['0', '1', '2', '3', '4', '5', '6', '7']
+us = ['u0', 'u1', 'u2']
 vec_test = [['0', '1', '2', '3'], ['0', '4', '2', '3', '5', '6'], ['3', '7', '5', '6']]
-model_test = gensim.models.Word2Vec(vec, window=6, min_count=1, size=2, batch_words=1)
+model_test = gensim.models.Word2Vec(vec_test, window=6, min_count=1, size=2)
+users_test = [sum(model_test.wv[u]) for u in vec_test]
+loca_test = [model_test.wv[l] for l in locs_test]
+x, y = zip(*users)
+z, t = zip(*loca_test)
+fig, ax = plt.subplots()
+ax.scatter(*zip(*loca_test))
+for j,txt in enumerate(locs_test):
+    ax.annotate(txt, (z[j],t[j]))
+ax.scatter(*zip(*users), marker='+')
+for i,txt in enumerate(us):
+    ax.annotate(txt, (x[i],y[i]))
+plt.show()
